5de70cc48c6b33444e276e62524a344ed2fdf908
abhirup-7325 <palabhirup07032005@gmail.com>
2025-02-25 08:13:29 +0530

    p8

--- Assignment_2/p8.java
+++ Assignment_2/p8.java
@@ -12,6 +12,70 @@ lecture slide to use as the data. Write a small main method to test your class; 
 four methods.  
  */
 
-public class p8 {
-    
-}
+ class ReverseCharSequence implements CharSequence {
+     private String original;
+     
+     public ReverseCharSequence(String original) {
+         this.original = original;
+     }
+     
+     @Override
+     public int length() {
+         return original.length();
+     }
+     
+     @Override
+     public char charAt(int index) {
+         return original.charAt(original.length() - 1 - index);
+     }
+     
+     @Override
+     public CharSequence subSequence(int start, int end) {
+         int originalStart = original.length() - end;
+         int originalEnd = original.length() - start;
+         
+         StringBuilder sub = new StringBuilder();
+         for (int i = originalStart; i < originalEnd; i++) {
+             sub.append(original.charAt(i));
+         }
+         
+         return new ReverseCharSequence(sub.toString());
+     }
+     
+     @Override
+     public String toString() {
+         StringBuilder reversed = new StringBuilder();
+         for (int i = original.length() - 1; i >= 0; i--) {
+             reversed.append(original.charAt(i));
+         }
+         return reversed.toString();
+     }
+ }
+ 
+ public class p8 {
+     public static void main(String[] args) {
+         String sentence = "Java does not allow friends...";
+         ReverseCharSequence reversedSequence = new ReverseCharSequence(sentence);
+         
+         System.out.println("Original length: " + sentence.length());
+         System.out.println("Reversed length: " + reversedSequence.length());
+         
+         System.out.println("\nOriginal first char: " + sentence.charAt(0));
+         System.out.println("Reversed first char: " + reversedSequence.charAt(0));
+         
+         System.out.println("\nOriginal string: " + sentence);
+         System.out.println("Reversed string: " + reversedSequence.toString());
+         
+         int start = 5;
+         int end = 15;
+         
+         CharSequence originalSub = new StringBuilder();
+         for (int i = start; i < end; i++) {
+             ((StringBuilder) originalSub).append(sentence.charAt(i));
+         }
+         
+         System.out.println("\nOriginal subsequence (5, 15): " + originalSub);
+         System.out.println("Reversed subsequence (5, 15): " + reversedSequence.subSequence(start, end));
+         System.out.println("ToString of reversed subsequence: " + reversedSequence.subSequence(start, end).toString());
+     }
+ }
